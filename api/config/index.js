c776363f0d687325628f22f53b17bfedd77c6f09
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2023-08-28 15:40:02 +0200

    config and dotenv setup complete

--- api/config/index.js
+++ api/config/index.js
@@ -1,1 +1,14 @@
-// For later use
+require('dotenv').config()
+const { createPool } = require('mysql')
+
+const connection = createPool({
+    host: process.env.dbHost,
+    user: process.env.dbUser,
+    password: process.env.dbPwd,
+    name: process.env.dbName,
+    port: process.env.dbPort,
+    multipleStatements: true,
+    connectionLimit: 30
+})
+
+module.exports = connection
