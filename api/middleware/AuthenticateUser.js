676a57c68afb03061a378469c943e813b0e69566
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2023-08-28 16:27:05 +0200

    Completed users section

--- api/middleware/AuthenticateUser.js
+++ api/middleware/AuthenticateUser.js
@@ -1,1 +1,27 @@
-// For later use
+const { sign, verify } = require("jsonwebtoken");
+require("dotenv").config();
+
+function createToken(user) {
+  return sign(
+    {
+      email: user.email,
+      password: user.password,
+    },
+    process.env.SECRET_KEY,
+    {
+      expiresIn: "1h",
+    }
+  );
+}
+
+function verifyAToken(token) {
+  try {
+    const dec = verify(token, process.env.SECRET_KEY);
+    return dec;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+}
+
+module.exports = { createToken, verifyAToken };
--- api/model/Users.js
+++ api/model/Users.js
@@ -1,1 +1,154 @@
-// For later use
+const db = require("../config");
+const { hash, compare, hashSync } = require("bcrypt");
+const { createToken } = require("../middleware/AuthenticateUser");
+
+class Users {
+  fetchUsers(req, res) {
+    const query = `
+            SELECT userID, firstName, lastName, username, gender, email, role, userProfile FROM Users;
+        `;
+    db.query(query, (err, results) => {
+      if (!err) {
+        res.json({
+          status: res.statusCode,
+          results,
+        });
+      } else {
+        throw (
+          err &&
+          res.json({
+            status: res.statusCode,
+            msg: "Something went wrong",
+          })
+        );
+      }
+    });
+  }
+  fetchUser(req, res) {
+    const query = `
+        SELECT userID, firstName, lastName, username, gender, email, role, userProfile FROM Users WHERE userID = ${req.params.id}
+        `;
+    db.query(query, (err, result) => {
+      if (!err) {
+        res.json({
+          status: res.statusCode,
+          result,
+        });
+      } else {
+        throw (
+          err &&
+          res.json({
+            status: res.statusCode,
+            msg: "Something went wrong",
+          })
+        );
+      }
+    });
+  }
+  async register(req, res) {
+    const data = req.body;
+    data.password = await hash(data.password, 17);
+    const user = {
+      email: data.email,
+      password: data.password,
+    };
+    const query = `
+            INSERT INTO Users SET ?
+        `;
+    db.query(query, [req.body], (err) => {
+      if (!err) {
+        let token = createToken(user);
+        res.json({
+          status: res.statusCode,
+          token,
+          msg: "User registered successfully",
+        });
+      } else {
+        res.json({
+          err,
+          msg: "An error occured",
+        });
+      }
+    });
+  }
+  async login(req, res) {
+    const { email, password } = req.body;
+    const query = `
+        SELECT firstName, lastName, username, gender, email, password, role, userProfile FROM Users WHERE email = ?
+    `;
+    db.query(query, [email], async (err, result) => {
+      if (err) {
+        res.json({
+          status: res.statusCode,
+          msg: "An error occured",
+        });
+      }
+      if (!result?.length) {
+        res.json({
+          status: res.statusCode,
+          msg: "You are providing the wrong email or password",
+        });
+      } else {
+        compare(password, result[0].password, (cErr, cRes) => {
+          if (cErr) throw cErr;
+          const token = createToken({
+            email,
+            password,
+          });
+          if (cRes) {
+            res.json({
+              msg: "Logged in successfully",
+              token,
+              cResult: cRes[0],
+            });
+          } else {
+            res.json({
+              status: res.statusCode,
+              msg: "Invalid login details",
+            });
+          }
+        });
+      }
+    });
+  }
+  banUser(req, res) {
+    const query = `
+        DELETE FROM Users WHERE userID = ${req.params.id}
+    `;
+    db.query(query, (err) => {
+      if (!err) {
+        res.json({
+          status: res.statusCode,
+          msg: "User banned successfully",
+        });
+      } else {
+        res.json({
+          status: res.statusCode,
+          msg: "Something went wrong",
+        });
+        throw err;
+      }
+    });
+  }
+  editUser(req, res) {
+    const query = `
+        UPDATE Users SET ? WHERE userID = ${req.params.id}
+    `;
+    db.query(query, [req.body], (err) => {
+      if (!err) {
+        res.json({
+          status: res.statusCode,
+          msg: "User record updated successfully",
+        });
+      } else {
+        res.json({
+          status: res.statusCode,
+          msg: "Something went wrong",
+        });
+        throw err;
+      }
+    });
+  }
+}
+
+module.exports = { Users }
