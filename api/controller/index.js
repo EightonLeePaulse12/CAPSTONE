6cd6eddc81fa5ed191f114978877412a5e9bf2d5
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2023-08-29 11:26:01 +0200

    Finished user routes

--- api/controller/index.js
+++ api/controller/index.js
@@ -1,1 +1,25 @@
-// For later use
+const express = require('express')
+const bodyParser = require('body-parser')
+const { verifyAToken } = require('../middleware/AuthenticateUser')
+const routes = express.Router()
+const { users, products, cart, transactions } = require('../model')
+
+// ========== User routes ==========
+routes.get('/users', (req, res)=>{
+    users.fetchUsers(req, res)
+})
+routes.get('/user/:id', (req, res)=>{
+    users.fetchUser(req, res)
+})
+routes.post('/register', bodyParser.json(), (req, res)=>{
+    users.register(req, res)
+})
+routes.post('/login', bodyParser.json(), (req, res)=>{
+    users.login(req, res)
+})
+routes.patch('/user/:id', bodyParser.json(), (req, res)=>{
+    users.editUser(req, res)
+})
+routes.delete('/user/:id', (req, res)=>{
+    users.banUser(req, res)
+})
--- api/model/index.js
+++ api/model/index.js
@@ -1,1 +1,11 @@
-// For later use
+const { Users } = require('./Users')
+const { Products } = require('./Products')
+const { Cart } = require('./Cart')
+const { Transactions } = require('./Transactions')
+
+module.exports = {
+    users: new Users(),
+    products: new Products(),
+    cart: new Cart(),
+    transactions: new Transactions()
+}
