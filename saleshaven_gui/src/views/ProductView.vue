e05da36774e7efd19437a338b1cad6343a40c322
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2023-08-31 08:42:15 +0200

    Fixed errors

---
+++ saleshaven_gui/src/views/ProductView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <button @click="logout">Log out</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        logout() {
+            this.$store.dispatch("logout")
+            this.$router.push("/")
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
