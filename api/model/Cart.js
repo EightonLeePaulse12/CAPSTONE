016fa730d420e15ed4095855b27fc10ae2b29b5d
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2023-09-06 10:13:04 +0200

    Fixed route and error handles

--- api/model/Cart.js
+++ api/model/Cart.js
@@ -1,63 +1,79 @@
-const db = require('../config')
+const db = require("../config");
 
-class Cart{
-    fetchCartItems(req, res){
-        const query = `
+class Cart {
+  fetchCartItems(req, res) {
+    const query = `
             SELECT cartID, userID, productID, quantity FROM Cart
-        `
-        db.query(query, (err, results)=>{
-            if (!err){
-                res.json({
-                    status: res.statusCode,
-                    results
-                })
-            } else{
-                throw err && res.json({
-                    status: res.statusCode,
-                    err,
-                    msg:"An error occured"
-                })
-            }
-        })
-    }
-    addToCart({userID, productID, quantity, total_price}, res){
-        const query = `
+        `;
+    db.query(query, (err, results) => {
+      if (!err) {
+        res.json({
+          status: res.statusCode,
+          results,
+        });
+      } else {
+        throw (
+          err &&
+          res.json({
+            status: res.statusCode,
+            err,
+            msg: "An error occured",
+          })
+        );
+      }
+    });
+  }
+  addToCart({ userID, productID, quantity, total_price }, res) {
+    try {
+      const query = `
             INSERT INTO Cart (userID, productID, quantity, total_price) VALUES(?, ?, ?, ?)
-        `
-        const data = [userID, productID, quantity, total_price]
-        db.query(query, data, (err)=>{
-            if (!err){
-                res.json({
-                    status: res.statusCode,
-                    msg:"Product added to cart successfully"
-                })
-            } else{
-                throw err && res.json({
-                    status: res.statusCode,
-                    msg:"Something went wrong"
-                })
-            }
-        })
+        `;
+      const data = [userID, productID, quantity, total_price];
+      db.query(query, data, (err) => {
+        if (!err) {
+          res.json({
+            status: res.statusCode,
+            msg: "Product added to cart successfully",
+          });
+        } else {
+          throw (
+            err &&
+            res.json({
+              status: res.statusCode,
+              msg: "Something went wrong",
+            })
+          );
+        }
+      });
+    } catch (e) {
+      console.error(e);
+      res
+        .status(500)
+        .json({ status: res.statusCode, msg: "Internal server error" });
     }
-    removeFromCart(req, res){
-        const query = `
+  }
+  removeFromCart(req, res) {
+    const query = `
             DELETE FROM Cart WHERE userID = ? AND productID = ?
-        `
-        const data = [userID, productID]
-        db.query(query, data, (err)=>{
-            if (!err){
-                res.json({
-                    status: res.statusCode,
-                    msg:"Product removed from cart"
-                })
-            } else{
-                throw err && res.json({
-                    status: res.statusCode,
-                    msg:"Something went wrong"
-                })
-            }
-        })
-    }
+        `;
+    const data = [userID, productID];
+    db.query(query, data, (err) => {
+      if (!err) {
+        res.json({
+          status: res.statusCode,
+          msg: "Product removed from cart",
+        });
+      } else {
+        throw (
+          err &&
+          res.json({
+            status: res.statusCode,
+            msg: "Something went wrong",
+          })
+        );
+      }
+    });
+  }
 }
 
-module.exports = { Cart }
+module.exports = { Cart };
